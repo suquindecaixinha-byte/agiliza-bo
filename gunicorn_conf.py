84a53cb92d4c985e2bc79ba71552d90fee771155
suquindecaixinha-byte <suquindecaixinha@gmail.com>
2025-11-27 15:54:27 -0300

    Delete gunicorn_conf.py

--- gunicorn_conf.py
+++
@@ -1,9 +0,0 @@
-# gunicorn_conf.py
-import multiprocessing
-
-# Configurações para o Render/Heroku
-bind = "0.0.0.0:10000"
-workers = 1  # No plano free, 1 worker é seguro
-worker_class = "uvicorn.workers.UvicornWorker"
-timeout = 120 # Tempo para a IA pensar sem dar timeout
-keepalive = 5
